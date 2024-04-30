b8a8e13fc99021cf032e7ed4933c562bec090860
ehkuitti <etsku.kuittinen@gmail.com>
2024-04-30 18:29:07 +0300

    Tehtävä 7.4 valmis

---
+++ Tehtävä 7.4/promise.js
@@ -0,0 +1,13 @@
+function myFirstPromise(shouldFail) {
+    return new Promise((resolve, reject) => {
+        if (shouldFail) 
+        {
+            reject("Fail");
+        }
+
+        else 
+        {
+            resolve("Success");
+        }
+    });
+}
